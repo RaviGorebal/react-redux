69f35c495a583729d8eeb36ec4a9446778a003be
Ravi Kumar G <ravikumarg@maveric-systems.com>
2020-07-07 12:37:45 +0530

    Updates

---
+++ precache-manifest.f816e0b8ea86011d308d99171ee498e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c1f5b80493ac46e9a6fb25d35841cb",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "fa75bc1bfdb4808d421b",
+    "url": "/react-redux/static/css/main.35cda0c4.chunk.css"
+  },
+  {
+    "revision": "538a00a93a6acda1acb6",
+    "url": "/react-redux/static/js/2.591c0643.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux/static/js/2.591c0643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa75bc1bfdb4808d421b",
+    "url": "/react-redux/static/js/main.4804e41a.chunk.js"
+  },
+  {
+    "revision": "996915a3bf219955dd88",
+    "url": "/react-redux/static/js/runtime-main.4d5a1b35.js"
+  }
+]);
